837488652bdc8c03bbdd7f62f3f20af8418001e0
4iar <me@4iar.com>
2017-06-28 22:13:52 +0100

    Implement readMatrixFromFile

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #define __CL_ENABLE_EXCEPTIONS
 
-#include <iostream>
 #include <CL/cl.hpp>
+#include <boost/algorithm/string.hpp>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <stdexcept>
@@ -9,7 +10,7 @@
 
 
 std::string readFile(std::string file_name);
-std::vector<float> readMatrixFromFile (std::string file_name, double columns, double rows);
+std::vector<float> readMatrixFromFile (std::string file_name);
 
 #ifndef DEVICE
 #define DEVICE CL_DEVICE_TYPE_DEFAULT
@@ -20,10 +21,32 @@ int main (int argc, char* argv[]) {
 }
 
 /**
- * \brief Read a matrix from a file
+ * \brief Read a matrix from a file and flatten it
  *
  */
-std::vector<float> readMatrixFromFile (std::string file_name, double columns, double rows) {
+std::vector<float> readMatrixFromFile (std::string file_name) {
+    // Read the matrix into a string, which has the format:
+    // n1,n2,n3,n4
+    // n5,n6,n7,n8
+    // n9,...
+    // (where line breaks are \n)
+    std::string raw_matrix = readFile(file_name);
+
+    // Flatten the matrix by splitting the strings using the , separator
+    // {n1,n2,n3,n4,n5,n6,n7,n8,n9...}
+    std::vector<std::string> flattened_matrix;
+    boost::split(flattened_matrix, raw_matrix, boost::is_any_of(","));
+
+    // Finally we convert the elements to floats
+    std::vector<float> matrix;
+    std::transform(flattened_matrix.begin(),
+                   flattened_matrix.end(),
+                   std::back_inserter(matrix),
+                   [](std::string element) {
+                       return std::stof(element);
+                   });
+
+    return matrix;
 }
 
 /**
